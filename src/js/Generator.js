2191c723683588fce91197d73dffb9d0f909d021
Nikita Pavets <nikitapavets@gmail.com>
2017-10-07 00:38:20 +0300

    fix

--- src/js/Generator.js
+++ src/js/Generator.js
@@ -49,13 +49,11 @@ export default class Generator {
   getDispersion(precision = 9) {
     const expectation = this.getExpectation();
 
-    return (this.list.reduce((sum, item) => sum + Math.pow(item, 2) - Math.pow(expectation, 2), 0) /(this.size - 1)).toFixed(precision);
+    return (this.list.reduce((sum, item) => sum + Math.pow(item - expectation, 2), 0) / (this.size - 1)).toFixed(precision);
   }
 
   getSKO(precision = 9) {
-    const expectation = this.getExpectation();
-
-    return (this.list.reduce((sum, item) => sum + Math.pow(item - expectation, 2), 0) /(this.size - 1)).toFixed(precision);
+    return Math.sqrt(this.getDispersion(precision)).toFixed(precision);
   }
 
   getUniformity(precision = 9) {
